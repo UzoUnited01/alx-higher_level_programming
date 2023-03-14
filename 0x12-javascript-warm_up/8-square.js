2ebce8b8c0a00768d0f6b662948531d2b5437e8d
UzoUnited01 <ofonyiri@gmail.com>
2023-03-14 11:55:57 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const arg = process.argv[2] * 1;
-if (!isNaN(arg)) {
-	const string = 'X';
-	for (let i = 0; i < arg; i++) {
-		console.log(string.repeat(arg));
-	}
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-	console.log('Missing size');
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
